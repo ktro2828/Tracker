4378365e3f4804fd243de7cbe30cc042bc423a02
kotarouetake <ktro310115@gmail.com>
2020-08-07 01:53:01 +0900

    1st commit

---
+++ tracker.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+
+import time
+
+import cv2
+
+
+class Tracker(object):
+    def __init__(self):
+        self.trakcer, self.tracker_name = self._select_tracker()
+        self.cap = cv2.VideoCapture(0)
+
+    def _select_tracker(self):
+        print('Which Tracker API do you use??')
+        print('0: Boosting')
+        print('1: MIL')
+        print('2: KCF')
+        print('3: MLD')
+        print('4: MedianFlow')
+        choice = input('Please select your tracker number: ')
+        print(choice)
+        print(type(choice))
+
+        if choice == 0:
+            tracker = cv2.TrackerBoosting_create()
+        elif choice == 1:
+            tracker = cv2.TrackerMIL_create()
+        elif choice == 2:
+            tracker = cv2.TrackerKCF_create()
+        elif choice == 3:
+            tracker = cv2.TrackerTLD_create()
+        elif choice == 4:
+            tracker = cv2.TrackerMedianFlow_create()
+        else:
+            raise ValueError('Tracker is undified!!')
+            return
+
+        self.tracker = tracker
+        self.tracker_name = str(tracker).split()[0][1:]
+
+        return self.tracker, self.tracker_name
+
+    def _iterate(self):
+        while True:
+            ret, frame = self.cap.read()
+            success, roi = self.tracker.update(frame)
+
+            (x, y, w, h) = tuple(map(int, roi))
+
+            if success:
+                p1 = (x, y)
+                p2 = (x + w, y + h)
+                cv2.rectangle(frame, p1, p2, color=(0, 255, 0), thickness=3)
+            else:
+                cv2.putText(frame, text='Tacking failed!!',
+                            org=(500, 400),
+                            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                            fontScale=1,
+                            color=(0, 0, 255),
+                            thickness=3)
+
+            cv2.putText(frame, text=self.tracker_name,
+                        org=(20, 600),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale=1,
+                        color=(0, 255, 0),
+                        thickness=3)
+
+            cv2.imshow(self.tracker_name, frame)
+            key = cv2.waitKey(1) & 0xff
+            if key == 27:
+                break
+
+    def start(self):
+        time.sleep(1)
+
+        ret, frame = self.cap.read()
+        roi = cv2.selectROI(frame, False)
+        self.tracker.init(frame, roi)
+
+        self._iterate()
+
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+
+def main():
+    tracker = Tracker()
+    tracker.start()
+
+
+if __name__ == '__main__':
+    main()
